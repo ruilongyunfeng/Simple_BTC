47e197d2f2db541f85d6f187d166533099af4652
ruilongyunfeng <ruijiezhi@163.com>
2018-11-25 14:43:44 +0800

    cli function

--- Block/CLI_Function.go
+++ Block/CLI_Function.go
@@ -20,3 +20,60 @@ func (cli *CLI) createWallet(nodeID string) {
 
 	fmt.Println(len(wallets.walletsMap))
 }
+
+func (cli *CLI) createGenesisBlockchain(address string, nodeID string) {
+	blockChain := CreateBlockchainWithGenesisBlock(address, nodeID)
+	defer blockChain.DB.Close()
+
+	utxoSet := &UTXOSet{blockChain}
+
+	utxoSet.ResetUTXOSet()
+}
+
+func (cli *CLI) addressList(nodeID string) {
+	fmt.Println("All wallet address:")
+
+	wallets, _ := NewWallets(nodeID)
+
+	for address, _ := range wallets.walletsMap {
+		fmt.Println(address)
+	}
+}
+
+func (cli *CLI) getBalance(address string, nodeID string) {
+	fmt.Println("address : " + address)
+
+	blockChain := BlockChainObject(nodeID)
+	defer blockChain.DB.Close()
+
+	utxoSet := &UTXOSet{blockChain}
+
+	amount := utxoSet.GetBalance(address)
+
+	fmt.Printf("%s have %d tokens.\n", address, amount)
+}
+
+func (cli *CLI) printChain(nodeID string) {
+	blockChain := BlockChainObject(nodeID)
+
+	defer blockChain.DB.Close()
+
+	blockChain.PrintChain()
+}
+
+func (cli *CLI) send(from []string, to []string, amount []string, nodeID string, mineNow bool) {
+	blockChain := BlockChainObject(nodeID)
+
+	utxoSet = &UTXOSet{blockChain}
+	defer blockChain.DB.Close()
+
+	if mineNow {
+		blockChain.MineNewBlock(from, to, amount, nodeID)
+		utxoSet.Update()
+	} else {
+		value, _ := strconv.Atoi(amount[0])
+		tx := NewSimpleTransaction(from[0], to[0], value, utxoSet, []*Transaction{}, nodeID)
+
+		sendTx(knowNodes[0], tx)
+	}
+}
